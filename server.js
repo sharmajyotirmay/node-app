53f328ce8d99a37f4bae0b4f7bce7fef437a8b93
Jyotirmay <jyotirmay.sharma@vwo.com>
2024-05-20 09:01:45 +0530

    node app

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
@@ -67,6 +66,6 @@ const server = http.createServer((req, res) => {
     });
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
 });
